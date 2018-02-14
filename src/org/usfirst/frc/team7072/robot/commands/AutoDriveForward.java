63b410f26553a7e67946aef8abf170253d9dfea2
unknown <thecoolflash7@gmail.com>
2018-02-13 19:26:10 -0600

    Added content to AutoDriveForward

--- src/org/usfirst/frc/team7072/robot/commands/AutoDriveForward.java
+++ src/org/usfirst/frc/team7072/robot/commands/AutoDriveForward.java
@@ -2,9 +2,7 @@ package org.usfirst.frc.team7072.robot.commands;
 
 import org.usfirst.frc.team7072.robot.Robot;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.*;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  *
@@ -15,51 +13,53 @@ public class AutoDriveForward extends PIDCommand {
 	private double travelDistance;
 	private double Circumference = 6 * Math.PI;
 	
-    public AutoDriveForward() {
-    	super(.5, 0, 0);
+    public AutoDriveForward(double distance) {
+    	super(0.5, 0, 0);
     	requires(Robot.driveTrain);
     	
+    	getPIDController().setAbsoluteTolerance(.1);
+    	getPIDController().setSetpoint(distance);
     	
-        // Use requires() here to declare subsystem dependencies
+    	
+    	// Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
-
+    
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	super.initialize();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	wheelRotations = Robot.driveTrain.getLeftEncoderPosition() / 4096;
-    	travelDistance = wheelRotations * Circumference;
-    	
+    	getPIDController().enable();
     	
     }
-
+    
+    
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return getPIDController().onTarget();
     }
 
-    // Called once after isFinished returns true
+    
+    @Override
     protected void end() {
+    	Robot.driveTrain.stopDrive();
     }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
-
+    
 	@Override
 	protected double returnPIDInput() {
-		// TODO Auto-generated method stub
-		return 0;
+		wheelRotations = Robot.driveTrain.getLeftEncoderPosition() / 4096;
+    	travelDistance = wheelRotations * Circumference;
+    	
+		return travelDistance;
 	}
 
 	@Override
 	protected void usePIDOutput(double output) {
 		// TODO Auto-generated method stub
+		Robot.driveTrain.tankDrive(output, output);
 		
 	}
 }
